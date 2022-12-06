55965ccf50a5d7d7a2e53d920c68a54770346982
jonpfluger <jonpfluger718@gmail.com>
2022-12-05 21:15:18 -0600

    seeds

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,15 +1,29 @@
 const sequelize = require('../config/connection');
-const { User } = require('../models');
+const { User, Review, Category } = require('../models');
 
 const userData = require('./userData.json');
+const reviewData = require('./reviewData.json');
+const categoryData = require('./categoryData.json');
 
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
 
-  await User.bulkCreate(userData, {
+  const users = await User.bulkCreate(userData, {
     individualHooks: true,
     returning: true,
   });
+
+  const categories = await Category.bulkCreate(categoryData, {
+    returning: true
+  })
+
+  for (const review of reviewData) {
+    await Review.create({
+      ...review,
+      user_id: users[Math.floor(Math.random() * users.length)].id,
+      category_id: categories[Math.floor(Math.random() * categories.length)].id,
+    })
+  }
 
   process.exit(0);
 };
