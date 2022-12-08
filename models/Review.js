800848f0ff0c2504d700bea291b8e889cec045f2
JenniferKiesler <111549041+JenniferKiesler@users.noreply.github.com>
2022-12-07 18:40:22 -0600

    review date created

--- models/Review.js
+++ models/Review.js
@@ -26,6 +26,11 @@ Review.init(
         type: DataTypes.STRING,
         allowNull: false,
     },
+    date_created: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: DataTypes.NOW
+    },
     user_id: {
         type: DataTypes.INTEGER,
         allowNull: false,
