63eb2a6513fe35eeb2e2b33b574831a7df961709
James <andritjm@gmail.com>
2022-12-12 20:49:39 -0600

    got most of cloudinary work done

---
+++ public/js/cloud.js
@@ -0,0 +1,23 @@
+require("dotenv").config();
+
+const cloudinary = require("cloudinary").v2;
+
+console.log(cloudinary.config().cloud_name);
+
+cloudinary.uploader
+    .upload()
+
+
+const uploadUrlInput = document.querySelector('[name="game-photo"]')
+    const uploadWidgetButton = document.getElementById("upload_widget")
+
+    var myWidget = cloudinary.createUploadWidget({
+        cloudName: "dih7bmipw",
+        uploadPreset: "HR_images"
+    }, 
+        (error, result) => {
+            if (!error && result && result.event === "success") {
+                console.log("Done! Here is the image info: ", result.info);
+            }
+        }
+    );
