9bac9293e7b25d9913c09c2000e5425f3f42fcf7
CRMclaughlin <chrisraymusik@gmail.com>
2022-12-12 21:26:46 -0600

    added new animations to nav links, and cards

--- public/js/animation.js
+++ public/js/animation.js
@@ -1,10 +1,32 @@
 document.addEventListener('DOMContentLoaded', () => {
-   anime({
-    targets: '#logo',
-    translateX: [-400, 0],
+    anime({
+        targets: '#logo',
+        translateX: [-400, 0],
+        easing: 'easeOutExpo',
+        duration: 1500,
+        delay: 1000,
+        opacity: [0, 1]
+    })
+})
+
+anime({
+    targets: 'nav a',
+    translateY: [-50, 0],
     easing: 'easeOutExpo',
     duration: 1500,
-    delay: 1000,
-    opacity: [0,1] 
-   }) 
+    delay: (el, i) => {
+        return 1000 + 100 * i;
+    },
+    opacity: [0, 1]
 })
+
+anime({
+    targets: '.myCustomCard',
+    scale: [
+      {value: .1, easing: 'easeOutSine', duration: 500},
+      {value: 1, easing: 'easeInOutQuad', duration: 1200}
+    ],
+    delay: anime.stagger(200, {grid: [14, 5], from: 'center'})
+  });
+
+//   '.myCustomCard'
