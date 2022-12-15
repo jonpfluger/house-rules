bc3fa16f6127ba606ae38090351a9abc150d8cd4
CRMclaughlin <chrisraymusik@gmail.com>
2022-12-14 19:11:19 -0600

    added buttons, functions, and event listeners

--- public/js/animation.js
+++ public/js/animation.js
@@ -1,25 +1,62 @@
 let headerAnimationsComplete = sessionStorage.getItem('headerAnimationsComplete')
 
 // const boardBtn = document.getElementById('boardBtn')
-// let cardBtn = document.querySelector('.cardBtn')
-// let videoBtn = document.querySelector('.videoBtn')
+// const cardBtn = document.getElementById('cardBtn')
+// const videoBtn = document.getElementById('videoBtn')
 
-// function aniCards() {
-//     anime({
-//         targets: '.animatedCard',
-//         translateX: [-400, 0],
-//         easing: 'easeOutExpo',
-//         duration: 1500,
-//         delay: 1000,
-//         opacity: [0, 1]
-//     })
+
+
+// function boardAni(event) {
+//     event.preventDefault()
+//     document.getElementById("boardBtn")
+// }
+
+// function cardAni(event) {
+//     event.preventDefault()
+//     document.getElementById("cardBtn")
+// }
+// function videoAni(event) {
+//     event.preventDefault()
+//     document.getElementById("videoBtn")
 // }
 
 
+// boardBtn.addEventListener('click', () => {
+//     anime({
+//         targets: '.animatedCard',
+//         translateX: [-270, 0],
+//         delay: (el, i) => {
+//             return 500 + 100 * i;
+//         },
+//         opacity: [0, 1]
+//       });
+// })
+
+// cardBtn.addEventListener('click', () => {
+//     anime({
+//         targets: '.animatedCard',
+//         translateX: [-270, 0],
+//         delay: (el, i) => {
+//             return 500 + 100 * i;
+//         },
+//         opacity: [0, 1]
+//       });
+// })
+
+// videoBtn.addEventListener('click', () => {
+//     anime({
+//         targets: '.animatedCard',
+//         translateX: [-270, 0],
+//         delay: (el, i) => {
+//             return 500 + 100 * i;
+//         },
+//         opacity: [0, 1]
+//       });
+// })
 
 document.addEventListener('DOMContentLoaded', () => {
-    if(!headerAnimationsComplete){
-            sessionStorage.setItem('headerAnimationsComplete', true)
+    if (!headerAnimationsComplete) {
+        sessionStorage.setItem('headerAnimationsComplete', true)
         anime({
             targets: '#logo',
             translateX: [-400, 0],
@@ -28,7 +65,7 @@ document.addEventListener('DOMContentLoaded', () => {
             delay: 1000,
             opacity: [0, 1]
         })
-        
+
         anime({
             targets: 'nav a',
             translateY: [-50, 0],
@@ -40,44 +77,19 @@ document.addEventListener('DOMContentLoaded', () => {
             opacity: [0, 1]
         })
     }
-    
+
 })
 
-// document.getElementById('#boardBtn').addEventListener('click', () => {
-//     aniCards()
-// })
-
+// working animation for cards
 anime({
     targets: '.animatedCard',
     scale: [
-      {value: .1, easing: 'easeOutSine', duration: 500},
-      {value: 1, easing: 'easeInOutQuad', duration: 1200}
+        { value: .1, easing: 'easeOutSine', duration: 500 },
+        { value: 1, easing: 'easeInOutQuad', duration: 1200 }
     ],
-    delay: anime.stagger(200, {grid: [14, 5], from: 'center'})
-  });
+    delay: anime.stagger(200, { grid: [14, 5], from: 'center' })
+});
 
-
-// moves right to left
-// anime({
-//     targets: '.animatedCard',
-//     translateX: 270,
-//     direction: 'alternate',
-//     easing: 'easeInOutSine',
-//     delay: function(el, i, l) {
-//       return i * 100;
-//     },
-//     endDelay: function(el, i, l) {
-//       return (l - i) * 100;
-//     }
-//   });
-
-// good for card shuffle
-
-// anime({
-//     targets: '.staggering-axis-grid-demo .el',
-//     translateX: anime.stagger(10, {grid: [14, 5], from: 'center', axis: 'x'}),
-//     translateY: anime.stagger(10, {grid: [14, 5], from: 'center', axis: 'y'}),
-//     rotateZ: anime.stagger([0, 90], {grid: [14, 5], from: 'center', axis: 'x'}),
-//     delay: anime.stagger(200, {grid: [14, 5], from: 'center'}),
-//     easing: 'easeInOutQuad'
-//   });
+// boardBtn.addEventListener("click", boardAni);
+// cardBtn.addEventListener("click", cardAni);
+// videoBtn.addEventListener("click", videoAni)
