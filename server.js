c20ac9f37768b55ff75363b2e4f9f49c563493ce
jonpfluger <jonpfluger718@gmail.com>
2022-12-14 22:29:44 -0600

    changed maxAge in server.js and fixed anchor tag for edit in dashboard handlebars

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const hbs = exphbs.create({ helpers });
 const sess = {
   secret: process.env.SESSION_SECRET,
   cookie: {
-    maxAge: 9000000,
+    maxAge: 900000,
   },
   resave: false,
   saveUninitialized: true,
